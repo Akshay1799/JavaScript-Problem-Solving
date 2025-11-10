29d9e7d4f7442692ff22815e215063701f37dd6a
Akshay Ladne <Kumawata283@gmail.com>
2025-11-10 12:50:39 +0530

    solved a problem

--- Medium/String & Patterns/05. compressString.js
+++ Medium/String & Patterns/05. compressString.js
@@ -1,19 +1,20 @@
-
 function compressString(str){
+    if(str.length === 0)return;
 
-    if(str.length === 0) return;
     let result = '';
+
     for (let i = 0; i < str.length; i++) {
         let char = str[i];
         let count = 1;
 
-        while (i<str.length-1 && str[i]===str[i+1]) {
+        while(i<str.length-1 && str[i] === str[i+1]){
             count++;
             i++;
         }
         result += char + count;
+        
     }
-    return result 
+    return result;
 }
 
 console.log(compressString("aabcccccaaa"));
